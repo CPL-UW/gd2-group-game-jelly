9d908ef207a88e6d63b841bba416306547034bb5
AndrewMeyers11 <ajmeyers4@wisc.edu>
2022-10-27 10:30:23 -0500

    Made players face the correct direction with movement

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -3,54 +3,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2Controller : MonoBehaviour {
+public class Player2Controller : MonoBehaviour
+{
 
     public float moveSpeed = 5f;
-    public float tilesMoved = 0f;
+    public int tilesMoved = 0;
 
     public bool p2Turn;
+
     public Transform movePoint;
-	public int SpeedInt;
+    public int SpeedInt;
 
     public LayerMask whatStopsMovement;
 
-    private void Start() {
+    public bool facingRight;
+
+
+    private void Start()
+    {
         movePoint.parent = null;
+
+        if (gameObject.transform.localScale.x > 0)
+        {
+            facingRight = true;
+        }
+        else
+        {
+            facingRight = false;
+        }
     }
-    
-    private void Update() {
+
+
+    private void Update()
+    {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         if (p2Turn == true)
         {
-            if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f) {
+            if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+            {
 
-                if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                
-                    if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)) {
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+                {
+                    // Set player direction
+                    if (Input.GetAxisRaw("Horizontal") > 0 && !facingRight)
+                    {
+                        Flip();
+                    }
+                    else if (Input.GetAxisRaw("Horizontal") < 0 && facingRight)
+                    {
+                        Flip();
+                    }
+
+                    // Move Player
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement))
+                    {
                         movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                        tilesMoved = tilesMoved + 1; //counts tiles
-                    } 
+                        tilesMoved++; //counts tiles
+                    }
 
-                } else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+                }
+                else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                {
 
-                    if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)) {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement))
+                    {
                         movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                        tilesMoved = tilesMoved + 1;
-                    } 
+                        tilesMoved++;
+                    }
                 }
 
-			    SpeedInt = 0;
-            } else {
-			    SpeedInt = 1;
+                SpeedInt = 0;
+            }
+            else
+            {
+                SpeedInt = 1;
             }
 
             if (tilesMoved == 5)
-                {
-                    //p2Turn = false;
-                    //Debug.Log("Player 1's turn over!");
-                    //tilesMoved = 0;
-                }
+            {
+                //p1Turn = false;
+                //Debug.Log("Player 1's turn over!");
+                //tilesMoved = 0;
             }
+        }
+    }
+
+
+    // Flips players x direction
+    private void Flip()
+    {
+        Vector3 currScale = gameObject.transform.localScale;
+        currScale.x *= -1;
+        gameObject.transform.localScale = currScale;
+        facingRight = !facingRight;
     }
 }
